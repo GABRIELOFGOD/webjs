76a7a3d5fdf3a7512cb6fd3c9645ab21c755d4f8
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-01-16 16:30:55 +0100

    still deploying

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use((req, res, next) => {
     if (isWhitelisted) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
         res.setHeader('Access-Control-Allow-Credentials', true);
     }
     // Pass to next layer of middleware
