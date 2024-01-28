d681a54ab41d0472c4af0117dd5cb6fd92e98454
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-01-28 23:40:54 +0100

    Changes

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -6,7 +6,7 @@ const createBlog = async (req, res) => {
     try {
         let poster = req.admin
         // =============== VALIDATING INPUT FIELDS ================== //
-        if(!title || !body || !req.file) return res.status(402).json({error: 'Your blog must have a title, context and an image', success: false})
+        if(!title || !body) return res.status(402).json({error: 'Your blog must have a title, context and an image', success: false})
 
         // ================== CHECKING IF BLOG EXISTS ====================== //
         let isTitle = await blogTitleFinder(title)
@@ -15,17 +15,16 @@ const createBlog = async (req, res) => {
         if(isTitle || isBody) return res.status(401).json({error: 'This blog already exists', success: false})
         
         // ================== SAVING BLOG TO DATABASE AND SENDING A SUCCESS RESPONSE =================== //
-        let imagePath = req.file.path
-        let newPath = imagePath.replace("\\", '/')
-        let otherPath = newPath.replace('\\', '/')
+        // let imagePath = req.file.path
+        // let newPath = imagePath.replace("\\", '/')
+        // let otherPath = newPath.replace('\\', '/')
         let newPost = {
             title,
             body,
-            image: `${req.protocol}://${req.headers.host}/${otherPath}`,
-            postedBy: poster._id
+            // image: `${req.protocol}://${req.headers.host}/${otherPath}`,
+            // postedBy: poster._id
         }
-        const newBlog = await blogCreator(newPost)
-        res.status(201).json({message: 'Blog has been posted successfully', success: true})
+        // res.status(201).json({message: 'Blog has been posted successfully', success: true})
     } catch (err) {
         res.status(501).json({error: 'Server not reached, check your internet and try again. If this error persists, kindly reach out to our support', success: false, errMsg: err});
     }
--- routes/blog.router.js
+++ routes/blog.router.js
@@ -7,18 +7,18 @@ const adminIdentifier = require('../middleware/identity')
 const router = express.Router()
 
 // =========== MULTER CONFIG =========== //
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images/')
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}_${file.originalname}`)
-    }
-  })
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, './public/images/')
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}_${file.originalname}`)
+//     }
+//   })
   
-  const upload = multer({storage})
+//   const upload = multer({storage})
 
-router.post('/post', upload.single('file'), authourizer, adminIdentifier, createBlog)
+router.post('/post', createBlog)
 router.get('/get', getBlogs)
 
 module.exports = router;
